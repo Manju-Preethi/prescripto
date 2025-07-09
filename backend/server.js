23cfff0c7e6b3bdda4e63729ddbf729231c549a9
Manju-Preethi <143400826+Manju-Preethi@users.noreply.github.com>
2025-07-09 23:30:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,22 +13,7 @@ connectDB()
 connectCloudinary()
 //middlewares
 app.use(express.json())
-const allowedOrigins = [
-  "https://prescripto-frontend-iuq8.onrender.com",
-  "https://prescripto-admin-s4nn.onrender.com"
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
-
+app.use(cors())
 
 //api end points
 app.use('/api/admin',adminRouter)
